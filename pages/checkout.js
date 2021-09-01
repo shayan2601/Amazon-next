c7150aa2bb00d67ad814d63cf92bfdf6ae041d56
Shayan Shuaib <shayan3742@gmail.com>
2021-09-01 19:15:11 +0500

    Another

--- pages/checkout.js
+++ pages/checkout.js
@@ -6,7 +6,7 @@ import { getBasketTotal, selectItems } from "./slices/basketSlice";
 import { useSession } from "next-auth/client";
 import CurrencyFormat from "react-currency-format";
 
-export default function checkout() {
+function checkout() {
   const basket = useSelector(selectItems);
   const [session] = useSession();
 
